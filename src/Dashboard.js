aa45bb575836de4356624af4240240b9378645e1
adarsh20200 <adarshrastogi.bfcinfotech@gmail.com>
2021-11-09 13:53:31 +0530

    first commit

---
+++ src/Dashboard.js
@@ -0,0 +1,360 @@
+import React, { Component } from 'react';
+import {
+  Button, TextField, Dialog, DialogActions, LinearProgress,
+  DialogTitle, DialogContent, TableBody, Table,
+  TableContainer, TableHead, TableRow, TableCell
+} from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
+import swal from 'sweetalert';
+const axios = require('axios');
+
+export default class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: '',
+      openProductModal: false,
+      openProductEditModal: false,
+      id: '',
+      desc: '',
+      page: 1,
+      search: '',
+      products: [],
+      pages: 0,
+      loading: false
+    };
+  }
+
+  componentDidMount = () => {
+    let token = localStorage.getItem('token');
+    if (!token) {
+      this.props.history.push('/login');
+    } else {
+      this.setState({ token: token }, () => {
+        this.getProduct();
+      });
+    }
+  }
+
+  getProduct = () => {
+    
+    this.setState({ loading: true });
+
+    let data = '?';
+    data = `${data}page=${this.state.page}`;
+    if (this.state.search) {
+      data = `${data}&search=${this.state.search}`;
+    }
+    axios.get(`http://localhost:2000/get${data}`, {
+      headers: {
+        'token': this.state.token
+      }
+    }).then((res) => {
+      this.setState({ loading: false, products: res.data.products, pages: res.data.pages });
+    }).catch((err) => {
+      swal({
+        text: err.response.data.errorMessage,
+        icon: "error",
+        type: "error"
+      });
+      this.setState({ loading: false, products: [], pages: 0 },()=>{});
+    });
+  }
+
+  deleteProduct = (id) => {
+    axios.post('http://localhost:2000/delete', {
+      id: id
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'token': this.state.token
+      }
+    }).then((res) => {
+
+      swal({
+        text: res.data.title,
+        icon: "success",
+        type: "success"
+      });
+
+      this.setState({ page: 1 }, () => {
+        this.pageChange(null, 1);
+      });
+    }).catch((err) => {
+      swal({
+        text: err.response.data.errorMessage,
+        icon: "error",
+        type: "error"
+      });
+    });
+  }
+
+  pageChange = (e, page) => {
+    this.setState({ page: page }, () => {
+      this.getProduct();
+    });
+  }
+
+  logOut = () => {
+    localStorage.setItem('token', null);
+    this.props.history.push('/');
+  }
+
+  onChange = (e) => {
+   
+    this.setState({ [e.target.name]: e.target.value }, () => { });
+   
+    
+  };
+
+  addProduct = () => {
+    const fileInput = document.querySelector("#fileInput");
+    const file = new FormData();
+
+    file.append('desc', this.state.desc);
+
+    axios.post('http://localhost:2000/add', file, {
+      headers: {
+        'content-type': 'multipart/form-data',
+        'token': this.state.token
+      }
+    }).then((res) => {
+
+      swal({
+        text: res.data.title,
+        icon: "success",
+        type: "success"
+      });
+
+      this.handleProductClose();
+      this.setState({ desc: '', }, () => {
+        this.getProduct();
+      });
+    }).catch((err) => {
+      swal({
+        text: err.response.data.errorMessage,
+        icon: "error",
+        type: "error"
+      });
+      this.handleProductClose();
+    });
+
+  }
+
+  updateProduct = () => {
+    const file = new FormData();
+    file.append('id', this.state.id);
+    file.append('desc', this.state.desc);
+
+
+    axios.post('http://localhost:2000/update', file, {
+      headers: {
+        'content-type': 'multipart/form-data',
+        'token': this.state.token
+      }
+    }).then((res) => {
+
+      swal({
+        text: res.data.title,
+        icon: "success",
+        type: "success"
+      });
+
+      this.handleProductEditClose();
+      this.setState({  desc: '', }, () => {
+        this.getProduct();
+      });
+    }).catch((err) => {
+      swal({
+        text: err.response.data.errorMessage,
+        icon: "error",
+        type: "error"
+      });
+      this.handleProductEditClose();
+    });
+
+  }
+
+  handleProductOpen = () => {
+    this.setState({
+      openProductModal: true,
+      id: '',
+     
+      desc: '',
+   
+    });
+  };
+
+  handleProductClose = () => {
+    this.setState({ openProductModal: false });
+  };
+
+  handleProductEditOpen = (data) => {
+    this.setState({
+      openProductEditModal: true,
+      id: data._id,
+    
+      desc: data.desc,
+   
+    });
+  };
+
+  handleProductEditClose = () => {
+    this.setState({ openProductEditModal: false });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.loading && <LinearProgress size={40} />}
+        <div>
+          <h2 style={{color:'green'}}>Dashboard</h2>
+          <Button
+           
+            variant="contained"
+            color=" secondary"
+            size="small"
+            onClick={this.handleProductOpen}
+          >
+            Add Product
+          </Button>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={this.logOut}
+          >
+            Log Out
+          </Button>
+        </div>
+
+        {/* Edit Product */}
+        <Dialog
+          open={this.state.openProductEditModal}
+          onClose={this.handleProductClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Edit Product</DialogTitle>
+          <DialogContent>
+            <TextField
+              id="standard-basic"
+              type="text"
+              autoComplete="off"
+              name="desc"
+              value={this.state.desc}
+              onChange={this.onChange}
+              placeholder="Description"
+              required
+            /><br />
+            <TextField
+              id="standard-basic"
+              type="number"
+              autoComplete="off"
+              name="price"
+              value={this.state.price}
+              onChange={this.onChange}
+              placeholder="Price"
+              required
+            /><br />
+          
+            
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={this.handleProductEditClose} color="danger">
+              Cancel
+            </Button>
+            <Button
+              disabled={ this.state.desc == '' }
+              onClick={(e) => this.updateProduct()} color="primary" autoFocus>
+              Edit Product
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        {/* Add Product */}
+        <Dialog
+          open={this.state.openProductModal}
+          onClose={this.handleProductClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Add Product</DialogTitle>
+          <DialogContent>
+          
+            <TextField
+              id="standard-basic"
+              type="text"
+              autoComplete="off"
+              name="desc"
+              value={this.state.desc}
+              onChange={this.onChange}
+              placeholder="Description"
+              required
+            /><br />
+      
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={this.handleProductClose} color="#002884'">
+              Cancel
+            </Button>
+            <Button
+              disabled={ this.state.desc == '' }
+              onClick={(e) => this.addProduct()} color="#002884'" autoFocus>
+              Add Product
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <br />
+
+        <TableContainer>
+         
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+         
+                <TableCell align="center">Description</TableCell>
+              
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.products.map((row) => (
+                <TableRow key={row.desc}>
+                 
+        
+                  <TableCell align="center">{row.desc}</TableCell>
+               
+                  <TableCell align="center">
+                    <Button
+                      className="button_style"
+                      variant="outlined"
+                      color="primary"
+                      size="small"
+                      onClick={(e) => this.handleProductEditOpen(row)}
+                    >
+                      Edit
+                  </Button>
+                    <Button
+                      className="button_style"
+                      variant="outlined"
+                      color="secondary"
+                      size="small"
+                      onClick={(e) => this.deleteProduct(row._id)}
+                    >
+                      Delete
+                  </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <br />
+          <Pagination count={this.state.pages} page={this.state.page} onChange={this.pageChange} color="primary" />
+        </TableContainer>
+
+      </div>
+    );
+  }
+}
